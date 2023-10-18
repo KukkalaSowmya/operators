b871657689442667b69b1095a99dc646588293e9
KukkalaSowmya <145061290+KukkalaSowmya@users.noreply.github.com>
2023-10-18 23:55:20 +0530

    Add files via upload

---
+++ SumSubMul.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class SumSubMul{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int a = Scanner.next.Int();
+    int b = Scanner.next.Int();
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+  }
+}
